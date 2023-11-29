62dc9b86032ff2b90a6231f08b78c90221fd1373
Scott Motte <mot@mot.la>
2023-11-29 09:44:05 -0800

    rebuild

---
+++ out/_next/static/7B0ZGcuZafbEmTV9CUxAj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/docs":["static/chunks/pages/docs-d38a43a4bd57b737.js"],"/docs/install":["static/chunks/pages/docs/install-f4ae0614b75d4982.js"],sortedPages:["/_app","/_error","/docs","/docs/install"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ out/_next/static/7B0ZGcuZafbEmTV9CUxAj/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdocs","\u002Fdocs\u002Finstall"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
