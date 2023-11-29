2090de6c67a2488ddd99f214b55e0063aefeb739
Scott Motte <mot@mot.la>
2023-11-28 17:11:03 -0800

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -15,25 +15,26 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
   images: {
-    unoptimized: true, // https://stackoverflow.com/questions/75785972/nextjs-export-folder-out-doesnt-have-index-html
+    loader: 'akamai',
+    path: ''
   },
-  reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    scrollRestoration: true,
-  },
-  exportPathMap: function () {
-    return {
-      '/docs/integrations/github-actions/nextjs.html': { page: '/docs/integrations/github-actions/nextjs' },
-      '/docs/integrations/supabase/nodejs.html': { page: '/docs/integrations/supabase/nodejs' },
-      '/docs/integrations/gitlab/ci-quickstart.html': { page: '/docs/integrations/gitlab/ci-quickstart' },
-      '/docs/integrations/vercel/vite.html': { page: '/docs/integrations/vercel/vite' },
-      '/docs/libraries.html': { page: '/docs' },
-      '/docs/libraries': { page: '/docs' },
-    };
-  },
+  assetPrefix: './'
+  //output: 'export', // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  //images: {
+  //  unoptimized: true, // https://stackoverflow.com/questions/75785972/nextjs-export-folder-out-doesnt-have-index-html
+  //},
+  //reactStrictMode: true,
+  //pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  //experimental: {
+  //  scrollRestoration: true,
+  //},
+  //exportPathMap: function () {
+  //  return {
+  //    '/docs/libraries.html': { page: '/docs' },
+  //    '/docs/libraries': { page: '/docs' },
+  //  };
+  //},
 }
 
 export default withSearch(withMDX(nextConfig))
