0e605e21d1ff3ca7b63c99505ca822e06b51c64c
Scott Motte <mot@mot.la>
2024-11-18 17:28:49 -0800

    fix link

--- src/components/FrameworksRust.jsx
+++ src/components/FrameworksRust.jsx
@@ -11,7 +11,7 @@ export function FrameworksRust() {
             Rocket
           </h3>
           <p className="mt-1 text-sm text-zinc-600 dark:text-zinc-400">Use dotenvx with Rocket</p>
-          <p className="mt-4"><Button href="/docs/frameworks/rocket-rs" variant="text" arrow="right">Rocket Guides</Button></p>
+          <p className="mt-4"><Button href="/docs/frameworks/rocket" variant="text" arrow="right">Rocket Guides</Button></p>
         </div>
         <RocketIcon className="h-9 w-9 text-[#C3454C]"/>
       </div>
