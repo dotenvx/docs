125c248049ab22b9ced34266bd142b3e1c3bd80f
Scott Motte <mot@mot.la>
2025-03-05 15:24:05 -0800

    className

--- src/components/icons/DotenvxProIcon.jsx
+++ src/components/icons/DotenvxProIcon.jsx
@@ -1,5 +1,5 @@
 export function DotenvxProIcon(props) {
   return (
-    <div class="ml-1 inline-block bg-[#ECD53F] text-black dark:bg-zinc-900 dark:text-[#ECD53F] font-bold px-2.5 py-1 text-sm italic rounded-sm -rotate-6">PRO</div>
+    <div className="ml-1 inline-block bg-[#ECD53F] text-black dark:bg-zinc-900 dark:text-[#ECD53F] font-bold px-2.5 py-1 text-sm italic rounded-sm -rotate-6">PRO</div>
   )
 }
