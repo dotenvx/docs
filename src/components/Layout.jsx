397bb1baf55c89f8c83e84162a3aa8f37068c573
Scott Motte <mot@mot.la>
2025-04-02 15:08:42 -0700

    use blue links

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -17,12 +17,12 @@ export function Layout({ children, sections = [] }) {
           className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex"
         >
           <div className="contents border-t border-yellow-300 lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-5 lg:pb-8 lg:pt-2 lg:dark:border-white/10 xl:w-80">
-            <div className="hidden lg:flex">
+            <div className="hidden lg:flex lg:items-center">
               <Link href="/" aria-label="Home">
-                <Logo className="w-10 h-10 mr-3" />
+                <Logo className="w-10 h-10 mr-5" />
               </Link>
-              <Link href="/docs" aria-label="Documentation" className="text-center rounded-full px-2 py-2 text-sm font-semibold text-zinc-500 hover:text-zinc-950 dark:text-zinc-400 dark:hover:text-zinc-50 active:bg-zinc-300 dark:active:bg-zinc-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-200 ease-in duration-150 mr-1">
-                Documentation
+              <Link href="/docs" aria-label="Documentation" className="text-blue-500 hover:text-zinc-950 dark:text-blue-400 dark:hover:text-zinc-50 underline font-bold tracking-tight text-sm">
+                Docs
               </Link>
             </div>
             <Header className="border-t border-yellow-300 dark:rounded-t-lg"/>
