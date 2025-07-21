79c777f205673b72e196af99b3aa1df35da99cbf
Scott Motte <mot@mot.la>
2025-07-21 10:37:37 -0600

    add button

---
+++ src/components/StripeButton.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+
+export function StripeButton() {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://js.stripe.com/v3/buy-button.js'
+    script.async = true
+    document.body.appendChild(script)
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+
+  return (
+    <stripe-buy-button
+      buy-button-id="buy_btn_1RnMKKFxNzl4s0PqnNw8giDp"
+      publishable-key="pk_live_51PAyvwFxNzl4s0PqjqzJypaxvKdMEaYePMGfUMppCnkqR6ZfHJ4KkWNPIzhbAukRRhiEHNjGcHwoNKevn2ay79V100Q2xNXwPl"
+    />
+  )
+}
