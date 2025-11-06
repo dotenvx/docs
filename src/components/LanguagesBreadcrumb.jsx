bc591cf005ff49672dd66cbc30af70dc54124235
Scott Motte <mot@mot.la>
2025-11-06 11:01:09 -0800

    update docs

--- src/components/LanguagesBreadcrumb.jsx
+++ src/components/LanguagesBreadcrumb.jsx
@@ -3,7 +3,7 @@ export function LanguagesBreadcrumb(props) {
     <>
       <div className="flex">
         <div className="flex-grow">
-          <h5><a href='/docs/languages'>Languages</a> {props.language ? <span>› <a href={props.url}>{props.language}</a></span> : <span></span>}</h5>
+          <h5><a href='/docs#languages'>Languages</a> {props.language ? <span>› <a href={props.url}>{props.language}</a></span> : <span></span>}</h5>
         </div>
         <div className="flex flex-shrink justify-end gap-2">
           {props.icon}
