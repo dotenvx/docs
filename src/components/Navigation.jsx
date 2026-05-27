998335c65fee8b0bedb3c5925c88f6325911c0cd
Scott Motte <mot@mot.la>
2026-05-26 20:22:32 -0700

    clearer name - command will still be `vlt`

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -341,7 +341,7 @@ export const defaultNavigation = [
     ]
   },
   {
-    title: 'Vlt ⛨',
+    title: 'Vault ⛨',
     links: [
       { title: 'Basics', href: '/docs/vlt/quickstart' },
       { title: 'Advanced', href: '/docs/vlt/advanced' },
@@ -370,7 +370,7 @@ export const cliNavigation = [
       { title: 'Keypair', href: '/docs/cli/keypair' },
       { title: 'Login', href: '/docs/cli/login' },
       {
-        title: 'Vlt ⛨',
+        title: 'Vault ⛨',
         href: '/docs/cli/vlt/introduction',
         links: [
           { title: 'Login', href: '/docs/cli/vlt/login' },
