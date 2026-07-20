e3b955d5e3bc16cbe68e4c1496809052df267d49
Scott Motte <mot@mot.la>
2026-07-20 12:14:07 -0700

    update videos

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,11 +1,16 @@
 export function Video(props) {
   return (
-    <div className="rounded-md border border-zinc-200 dark:border-zinc-700">
-      <video controls poster={props.poster} className="w-full aspect-video rounded-md m-0 shadow-lg">
-        <source src={props.mp4} type="video/mp4" />
-        <source src={props.webm} type="video/webm" />
-        Download the <a href={props.mp4}>mp4</a> <a href={props.webm}>webm</a> video.
-      </video>
-    </div>
+    <section className="not-prose mt-12 border-t border-zinc-900/10 pt-8 dark:border-white/10">
+      <h2 className="mb-4 text-base font-semibold text-zinc-900 dark:text-white">
+        Watch it in action
+      </h2>
+      <div className="rounded-md border border-zinc-200 dark:border-zinc-700">
+        <video controls poster={props.poster} className="m-0 aspect-video w-full rounded-md shadow-lg">
+          <source src={props.mp4} type="video/mp4" />
+          <source src={props.webm} type="video/webm" />
+          Download the <a href={props.mp4}>mp4</a> <a href={props.webm}>webm</a> video.
+        </video>
+      </div>
+    </section>
   )
 }
